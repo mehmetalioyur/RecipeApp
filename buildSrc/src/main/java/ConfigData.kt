ebceef4ef5033e9356168bf4fb534016031079ec
Mehmet Ali Öyür <mehmetalioyur@gmail.com>
2025-09-22 07:03:20 +0300

    add app icon

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -2,6 +2,6 @@ object ConfigData {
     const val compileSdkVersion = 35
     const val minSdkVersion = 23
     const val targetSdkVersion = 35
-    const val versionCode = 1
+    const val versionCode = 2
     const val versionName = "1.0"
 }
